40cf932a46fcf51a152f7e72c0283b5445e242f2
Arunika Srivastava <aru26srivastava001@gmail.com>
2023-01-22 23:40:32 +0530

    Commited files

---
+++ src/studentapp/Main.java
@@ -0,0 +1,29 @@
+package studentapp;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+                                                                                 
+        System.out.println("Enter number of new students to enroll:");           //Ask how many students you want to add
+        Scanner in = new Scanner(System.in);
+
+        int numOfStudents = in.nextInt();
+    
+        Student[] student = new Student[numOfStudents];                            //Creating array of objects , array of type Student
+    
+        for(int i=0;i<numOfStudents;i++)                                           //Create n number of new students
+        {
+         student[i]= new Student();
+         student[i].enroll();
+         student[i].setStudentId();
+         student[i].payTution();
+         student[i].showStatus();
+        }
+        
+        for(int j=0;j<numOfStudents;j++)
+        {
+            System.out.println(student[j].toString());
+        }
+    }
+}
---
+++ src/studentapp/Student.java
@@ -0,0 +1,75 @@
+package studentapp;
+
+import java.util.Scanner;
+
+public class Student {
+    private String firstname;
+    private String lastname;
+    private int gradeYear;
+    private String studentID;
+    private String courses = "";
+    private int tutionbalance;
+    private static int costOfCourse = 600;
+    private static int id = 1000;
+
+    Student(){                                                //Constructor: prompts user to enter student's name,year
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter student firstname");
+        this.firstname = in.nextLine();
+
+        System.out.println("Enter student lastname");
+        this.lastname = in.nextLine();
+
+        System.out.print("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class grade: ");
+        this.gradeYear = in.nextInt();
+
+    }
+
+    void setStudentId() {                                                           //Generate an id
+                                                                                    
+        id++;                                                                       //increment of id with each student
+        this.studentID = gradeYear + "" + id;                                      //Grade level + ID
+
+    }
+
+    public void enroll(){                                                          //Enroll in courses
+        
+        do {
+        System.out.print("Enter course name to enroll (Q to quit): ");
+
+        Scanner in = new Scanner(System.in);
+        String course = in.nextLine();
+
+        if(!course.equals("Q")){
+            courses = courses + "" + course;
+            tutionbalance = tutionbalance + costOfCourse;
+        }
+        else{
+            break;
+        }
+    } while(1!=0);
+
+       System.out.println("TUTION BALANCE: " + tutionbalance);
+    }
+
+    public void payTution()
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the amount you want to pay: ");
+
+        int payment = input.nextInt();
+        tutionbalance = tutionbalance - payment;
+
+        System.out.println("Thank you for yor payment of $" + payment);
+
+    }
+
+    public void showStatus()                                              //show status
+    {
+    System.out.println("Name: "+firstname + " " + lastname + "\nGrade Year: " + gradeYear + "\nStudent ID: " + studentID + "\nENROLLED IN: " + courses +  "\nYour balance is: $"+tutionbalance);
+    }
+     
+}
+
+    
